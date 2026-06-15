6fdd0a089ae0339c2734db54bd5208334c9dbf9b
Noella UWAYO <noella.uwayo@minecofin.gov.rw>
2026-06-15 10:39:18 +0200

    Lab 1.1 Generics & Collections completed

---
+++ week1_advanced_java_core_foundations/lab1_1_generics_collections/src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+
+        WarehouseStore<Product> store = new WarehouseStore<>();
+
+        store.addItem(new Product("1", "Laptop", "Electronics", 800));
+        store.addItem(new Product("2", "Phone", "Electronics", 500));
+        store.addItem(new Product("3", "Table", "Furniture", 200));
+
+        System.out.println("ALL PRODUCTS:");
+        store.displayAll();
+
+        System.out.println("\nFILTER ELECTRONICS:");
+        System.out.println(store.findByCategory("Electronics"));
+
+        store.removeItem("2");
+
+        System.out.println("\nAFTER REMOVAL:");
+        store.displayAll();
+    }
+}
