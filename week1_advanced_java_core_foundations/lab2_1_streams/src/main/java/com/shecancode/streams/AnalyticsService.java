3cc430a602247c70407f86513d7fc30adf243112
Noella UWAYO <noella.uwayo@minecofin.gov.rw>
2026-06-24 14:58:39 +0200

    Completed Lab 2.1 Streams and Collectors

--- week1_advanced_java_core_foundations/lab2_1_streams/src/main/java/com/shecancode/streams/AnalyticsService.java
+++ week1_advanced_java_core_foundations/lab2_1_streams/src/main/java/com/shecancode/streams/AnalyticsService.java
@@ -1,6 +1,8 @@
 package com.shecancode.streams;
 
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class AnalyticsService {
 
@@ -18,5 +20,58 @@ public class AnalyticsService {
                 .filter(item -> item.getQuantity() > 5)
                 .map(item -> item.revenue())
                 .reduce(0.0, Double::sum);
+    }
+
+    public List<String> topNProductsByRevenue(List<Order> orders, int n) {
+
+        return orders.stream()
+                .flatMap(order -> order.getItems().stream())
+
+                .collect(Collectors.groupingBy(
+                        item -> item.getProduct().getName(),
+                        Collectors.summingDouble(LineItem::revenue)
+                ))
+
+                .entrySet()
+                .stream()
+
+                .sorted((a, b) -> Double.compare(b.getValue(), a.getValue()))
+
+                .limit(n)
+
+                .map(Map.Entry::getKey)
+
+                .toList();
+    }
+
+    public Map<String, Long> countItemsByCategory(List<Order> orders) {
+
+        return orders.stream()
+                .flatMap(order -> order.getItems().stream())
+                .collect(Collectors.groupingBy(
+                        LineItem::getCategory,
+                        Collectors.counting()
+                ));
+    }
+
+    public Map<Boolean, List<Order>> partitionOrders(List<Order> orders) {
+
+        return orders.stream()
+                .collect(Collectors.partitioningBy(
+                        Order::isDelivered
+                ));
+    }
+
+    public Map<String, Double> productAveragePrices(List<Order> orders) {
+
+        return orders.stream()
+                .flatMap(order -> order.getItems().stream())
+                .collect(Collectors.toMap(
+                        item -> item.getProduct().getId(),
+                        item -> item.getProduct().getPrice(),
+
+                        (existingPrice, newPrice) ->
+                                (existingPrice + newPrice) / 2
+                ));
     }
 }
